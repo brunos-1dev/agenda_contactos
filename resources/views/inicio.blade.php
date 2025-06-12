252fd75bd981fb9ee3578e015be3e54dfdbbb0bd
Leandro Diaz <diazleandron@gmail.com>
2025-06-12 18:14:12 -0300

    leandro

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -23,16 +23,19 @@
 <div class="text-center my-5">
     <div class="bg-primary text-white py-2 w-100">
     <h1 class="text-center m-0">
-        Bienvenido a SIDAP - Sistema Interno de Datos del Personal
+        Sistema de Registro de Usuarios (SiRUs)
     </h1>
 </div>
 <br>
+<br>
     <h4 class="mb-4">SELECCIONE UNA OPCIÓN PARA CONTINUAR NAVEGANDO:</h4>
+    <br>
     <div class="d-flex justify-content-center gap-4 flex-wrap">
         <a href="{{ route('contacts.index') }}" class="btn btn-primary menu-btn">
             <img src="{{ asset('images/persona3.png') }}" alt="Personas" width="200" height="200" class="mb-2"> PERSONAS</a>
         <a href="{{ route('departamentos.index') }}" class="btn btn-primary menu-btn">
-            <img src="{{ asset('images/depto2.png') }}" alt="Personas" width="200" height="200" class="mb-2">DEPARTAMENTOS</a>
-    </div>
+            <img src="{{ asset('images/depto2.png') }}" alt="Departamentos" width="200" height="200" class="mb-2">DEPARTAMENTOS</a>
+     <a href="{{ route('departamentos.index') }}" class="btn btn-primary menu-btn">
+            <img src="{{ asset('images/logo app corto.png') }}" alt="Aplicaciones" width="200" height="200" class="mb-2">APLICACIONES</a>
 </div>
 @endsection
